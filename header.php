cde1fd88def57a98c09d84c1218f2218439d99d1
Pere Gual Rodrigo <gualrodrigopere@gmail.com>
2020-03-09 21:36:48 +0100

    Primer comit header

---
+++ header.php
@@ -0,0 +1,43 @@
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" href="#"><img class="nav-logo" src="public/img/logo.jpg"></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active" href="#"><span class="nav-span">Portada</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#"><span class="nav-span">Presentació</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#"><span class="nav-span">Fes-te'n soci@</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#"><span class="nav-span">Fer donatiu</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#"><span class="nav-span">Revista Nemus</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#"><span class="nav-span">Activitats</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#"><span class="nav-span">Fòssils Castellonencs</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#"><span class="nav-span">Cessió Fòssils a Museus</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#"><span class="nav-span">Webs/Blocs d'interès</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#"><span class="nav-span">Mapa Web</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#"><span class="nav-span">Contacte</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#"><span class="nav-span">Política de Cookies</span></a>
+        </li>
+    </ul>
+</nav>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <link rel="shortcut icon" type="image/x-icon" href="public/img/ateneudenatura.ico">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="public/css/global/bootstrap.min.css">
+        <!-- Header CSS -->
+        <link rel="stylesheet" href="public/css/header.css">
+
+        <title>Web Ateneu de Natura</title>
+    </head>
+    <body>
+        <?php require_once './header.php'; ?>
+    </body>
+</html>
